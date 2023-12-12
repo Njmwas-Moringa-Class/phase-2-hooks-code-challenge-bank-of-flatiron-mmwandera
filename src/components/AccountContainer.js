eb6f7b64bf000bc2f127571b161a38ef5a3426bd
mmwandera <mmwandera74@gmail.com>
2023-12-12 12:59:38 +0300

    search feature complete

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,29 +4,44 @@ import AddTransactionForm from "./AddTransactionForm";
 import Search from "./Search";
 
 function AccountContainer() {
-  // State to store the list of transactions
+  
   const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
 
   // Fetch transactions from the backend URL
   useEffect(() => {
     fetch("http://localhost:8001/transactions")
       .then(response => response.json())
-      .then(data => setTransactions(data))
+      .then(data => {
+        setTransactions(data);
+        setFilteredTransactions(data);
+      })
       .catch(error => console.error("Error fetching transactions:", error));
   }, []);
 
   // Function to add a new transaction to the list
   const addTransaction = (newTransaction) => {
     setTransactions((prevTransactions) => [...prevTransactions, newTransaction]);
+
+    // Update filtered transactions to include the new transaction
+    setFilteredTransactions((prevTransactions) => [...prevTransactions, newTransaction]);
   };
+
+  const handleSearch = (term) => {
+    // Filter transactions based on the search term
+    const filtered = transactions.filter(transaction =>
+      transaction.description.toLowerCase().includes(term.toLowerCase())
+    );
+    setFilteredTransactions(filtered);
+  }
 
   return (
     <div>
-      <Search />
+      <Search onSearch={handleSearch}/>
       {/* Pass transactions and addTransaction function as props */}
       <AddTransactionForm transactions={transactions} addTransaction={addTransaction} />
       {/* Pass transactions as a prop */}
-      <TransactionsList transactions={transactions} />
+      <TransactionsList transactions={filteredTransactions} />
     </div>
   );
 }
